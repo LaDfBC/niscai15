07c9b23d1c7034bf336e1abe40b7dac5c033894f
Jeff <jeffborwey@gmail.com>
2015-11-14 17:03:43 -0600

    dir structure

---
+++ nisc-go-coop/games/anarchy/Strategy/Building/Warehouse.java
@@ -0,0 +1,7 @@
+package games.anarchy.Strategy.Building;
+
+/**
+ * Created by Jeffrey on 11/14/2015.
+ */
+public class Warehouse {
+}
---
+++ nisc-go-coop/games/anarchy/Strategy/Heuristic/WeatherDefense.java
@@ -0,0 +1,7 @@
+package games.anarchy.Strategy.Heuristic;
+
+/**
+ * Created by Jeffrey on 11/14/2015.
+ */
+public class WeatherDefense {
+}
