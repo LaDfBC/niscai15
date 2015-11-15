877eaa7971c1f82ad8d67c5650d759cc31ed511f
unknown <jkurtz@jkurtz.nisc.lan>
2015-11-15 02:36:51 +0000

    WarehouseUtilities.getClosestWarehouse

--- nisc-go-coop/games/anarchy/Strategy/Building/WarehouseUtilities.java
+++ nisc-go-coop/games/anarchy/Strategy/Building/WarehouseUtilities.java
@@ -1,7 +1,10 @@
 package games.anarchy.Strategy.Building;
 
+import com.sun.javafx.beans.annotations.NonNull;
 import games.anarchy.Building;
 import games.anarchy.Warehouse;
+
+import java.util.List;
 
 /**
  * Created by squeaky on 11/14/15.
@@ -10,4 +13,18 @@ public class WarehouseUtilities {
     public static int exposureAddedToIgnite(Warehouse ignitingWarehouse, Building buildingToBurn) {
         return Math.abs(ignitingWarehouse.x - buildingToBurn.x) + Math.abs(ignitingWarehouse.y - buildingToBurn.y);
     }
+
+    public static Warehouse getClosestWarehouse(@NonNull Building building, @NonNull List<Warehouse> warehouses){
+        if(warehouses.isEmpty()){
+            return null;
+        }
+        Warehouse closest = warehouses.get(0);
+        for(Warehouse warehouse : warehouses){
+            if(exposureAddedToIgnite(warehouse, building) < exposureAddedToIgnite(closest, building)){
+                closest = warehouse;
+            }
+        }
+        return closest;
+    }
+
 }
